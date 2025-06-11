aec1ee89caf217113f00d1815b5e84893e1ee293
Abdallah Hashesh <116919038+abdallahsh@users.noreply.github.com>
2025-06-11 23:18:56 +0300

    Update browser.service.js

--- services/browser.service.js
+++ services/browser.service.js
@@ -26,7 +26,7 @@ export class BrowserService {
         try {
             const { page, browser } = await connect({
                 args: config.browser.args,
-                headless: false,
+                headless: true,
                 turnstile: true,
                 customConfig: {
                     ignoreDefaultArgs: ['--enable-automation'],
